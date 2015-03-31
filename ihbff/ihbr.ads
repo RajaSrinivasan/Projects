06d7047700a6fb85a79e0928812d4a5db71f5d77
R Srinivasan <s@srin.me>
2015-03-31 05:45:53 -0400

    Ada project framework
    tweak the spec

--- ihbff/ihbr.ads
+++ ihbff/ihbr.ads
@@ -21,7 +21,7 @@ package Ihbr is
    
    type Data_Rec_Type is array(Natural range <>) of
            Interfaces.Unsigned_8 ;
-   
+      
    type Ihbr_Record_Type ( Rectype : Rectype_Type ) is
       record
 	 case Rectype is
@@ -42,23 +42,21 @@ package Ihbr is
 	 end case ;
       end record ;
    
-   procedure Read( Stream : not null access Ada.Streams.Root_Stream_Type'Class ;
-		   Item : out Ihbr_Record_Type ) ;
-   for Ihbr_Record_Type'Read use Read ;
-   procedure Write( Stream : not null access Ada.Streams.Root_Stream_Type'Class ;
-		    Item : in Ihbr_Record_Type ) ;
-   for Ihbr_Record_Type'Write use Write ;
+   type File_Type is private ;
    
-   type Ihbr_File_Type is
+ 
+   procedure Open( Name : String ;
+		   File : out file_type ) ;
+   procedure Close( File : in out file_type ) ;
+   procedure GetNext( File : in out file_type ;
+		      Rec : out Ihbr_Record_Type ) ;
+   procedure PutNext( File : in out file_type ;
+                      Rec : Ihbr_Record_Type ) ;
+private
+   type file_rec_type is
       record
 	 File : Ada.Text_Io.File_Type ;
 	 Current_Line : Integer ;
       end record ;
-   procedure Open( Name : String ;
-		   File : out Ihbr_File_Type ) ;
-   procedure Close( File : in out Ihbr_File_Type ) ;
-   procedure GetNext( File : in out Ihbr_File_Type ;
-		      Rec : out Ihbr_Record_Type ) ;
-   procedure PutNext( File : in out Ihbr_File_Type ;
-		      Rec : Ihbr_Record_Type ) ;
+   type file_type is access all file_rec_type ;
 end Ihbr ;
