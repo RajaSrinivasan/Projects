9d37e64b7fb22fa8dea8dc9895633af444c1515d
R Srinivasan <s@srin.me>
2015-05-12 07:30:43 -0400

    Pretty Printed

--- lib/csv.adb
+++ lib/csv.adb
@@ -1,45 +1,41 @@
-with Text_Io ; use Text_Io ;
+with Text_IO; use Text_IO;
 
 package body Csv is
-   function Open
-     (Name : String;
-      Separator : String)
-      return File_Type
-   is
-      File : File_Type := new File_Object_Type ;
+   function Open (Name : String; Separator : String) return File_Type is
+      File : File_Type := new File_Object_Type;
    begin
 
-      Gnat.Awk.Add_File( Name , File.Session ) ;
-      Gnat.Awk.Set_Field_Separators( Separator , File.Session ) ;
-      Gnat.Awk.Open( Session => File.Session ) ;
-      Gnat.Awk.Get_Line( Session => File.Session ) ;
-      File.No_Columns := Integer(Gnat.Awk.Number_Of_Fields(File.Session)) ;
-      if Debug
-      then
-         Text_Io.Put("Number of Fields ");
-         Text_Io.Put_Line( Integer'Image(Integer(Gnat.Awk.Number_Of_Fields( File.Session ) ) ) ) ;
-      end if ;
-      for Fld in 1..File.No_Columns
-      loop
+      GNAT.AWK.Add_File (Name, File.Session);
+      GNAT.AWK.Set_Field_Separators (Separator, File.Session);
+      GNAT.AWK.Open (Session => File.Session);
+      GNAT.AWK.Get_Line (Session => File.Session);
+      File.No_Columns := Integer (GNAT.AWK.Number_Of_Fields (File.Session));
+      if Debug then
+         Text_IO.Put ("Number of Fields ");
+         Text_IO.Put_Line
+           (Integer'Image
+              (Integer (GNAT.AWK.Number_Of_Fields (File.Session))));
+      end if;
+      for Fld in 1 .. File.No_Columns loop
          declare
-            use String_Vectors_Pkg ;
-            Columnname : String := Gnat.Awk.Field(Gnat.Awk.Count(Fld),File.Session) ;
-            Oldentry : String_Vectors_Pkg.Cursor ;
+            use String_Vectors_Pkg;
+            Columnname : String :=
+              GNAT.AWK.Field (GNAT.AWK.Count (Fld), File.Session);
+            Oldentry : String_Vectors_Pkg.Cursor;
          begin
-            if Debug
-            then
-               Put("Field "); Put_Line( Columnname ) ;
+            if Debug then
+               Put ("Field ");
+               Put_Line (Columnname);
             end if;
-            Oldentry := String_Vectors_Pkg.Find( File.Field_Names , Columnname ) ;
-            if Oldentry /= String_Vectors_Pkg.No_Element
-            then
-               raise Duplicate_Column ;
-            end if ;
-            String_Vectors_Pkg.Append( File.Field_Names , Columnname ) ;
-         end ;
-      end loop ;
-      File.Current_Line := 0 ;
-      return File ;
+            Oldentry := String_Vectors_Pkg.Find (File.Field_Names, Columnname);
+            if Oldentry /= String_Vectors_Pkg.No_Element then
+               raise Duplicate_Column;
+            end if;
+            String_Vectors_Pkg.Append (File.Field_Names, Columnname);
+         end;
+      end loop;
+      File.Current_Line := 0;
+      return File;
    end Open;
 
    -----------
@@ -48,8 +44,8 @@ package body Csv is
 
    procedure Close (File : in out File_Type) is
    begin
-      String_Vectors_Pkg.Clear( File.Field_Names ) ;
-      Gnat.Awk.Close( File.session ) ;
+      String_Vectors_Pkg.Clear (File.Field_Names);
+      GNAT.AWK.Close (File.Session);
    end Close;
 
    ----------------
@@ -58,32 +54,28 @@ package body Csv is
 
    function No_Columns (File : File_Type) return Integer is
    begin
-      return File.No_Columns ;
+      return File.No_Columns;
    end No_Columns;
 
-   function Field_Name
-     (File : File_Type;
-      Column : Integer)
-      return String
-   is
-      Ptr : String_Vectors_Pkg.Cursor ;
+   function Field_Name (File : File_Type; Column : Integer) return String is
+      Ptr : String_Vectors_Pkg.Cursor;
    begin
-      Ptr := String_Vectors_Pkg.To_Cursor(File.Field_Names,Column);
-      return String_Vectors_Pkg.Element( Ptr ) ;
+      Ptr := String_Vectors_Pkg.To_Cursor (File.Field_Names, Column);
+      return String_Vectors_Pkg.Element (Ptr);
    end Field_Name;
 
    procedure Get_Line (File : in out File_Type) is
    begin
-      Gnat.Awk.Get_Line( Session => File.Session ) ;
-      if File.No_Columns /= Integer(Gnat.Awk.Number_Of_Fields(File.Session))
+      GNAT.AWK.Get_Line (Session => File.Session);
+      if File.No_Columns /=
+        Integer (GNAT.AWK.Number_Of_Fields (File.Session))
       then
-         if DEBUG
-         then
-            Put("Invalid line in input file");
-            Put_Line( Gnat.Awk.Field( 0 , File.Session ) );
-         end if ;
-      end if ;
-      File.Current_Line := File.Current_Line + 1 ;
+         if Debug then
+            Put ("Invalid line in input file");
+            Put_Line (GNAT.AWK.Field (0, File.Session));
+         end if;
+      end if;
+      File.Current_Line := File.Current_Line + 1;
    end Get_Line;
 
    function Line_No (File : File_Type) return Integer is
@@ -91,29 +83,21 @@ package body Csv is
       return File.Current_Line;
    end Line_No;
 
-   function End_Of_File
-     (File : File_Type)
-      return Boolean
-   is
+   function End_Of_File (File : File_Type) return Boolean is
    begin
-      return Gnat.Awk.End_Of_File(File.Session) ;
+      return GNAT.AWK.End_Of_File (File.Session);
    end End_Of_File;
 
-   function Field
-     (File : File_Type;
-      Column : Integer)
-      return String
-   is
+   function Field (File : File_Type; Column : Integer) return String is
    begin
-      return Gnat.Awk.Field( Gnat.Awk.Count( Column ) , File.Session ) ;
+      return GNAT.AWK.Field (GNAT.AWK.Count (Column), File.Session);
    end Field;
 
-   function Field( File : File_Type ; Name : String )
-                 return String is
-      Index : String_Vectors_Pkg.Extended_Index ;
+   function Field (File : File_Type; Name : String) return String is
+      Index : String_Vectors_Pkg.Extended_Index;
    begin
-      Index := String_Vectors_Pkg.Find_Index( File.Field_Names , Name ) ;
-      return Field( File , Integer(Index) ) ;
-   end Field ;
+      Index := String_Vectors_Pkg.Find_Index (File.Field_Names, Name);
+      return Field (File, Integer (Index));
+   end Field;
 
 end Csv;
--- lib/csv.ads
+++ lib/csv.ads
@@ -1,39 +1,32 @@
-with Interfaces ;
-with Ada.Strings.Unbounded ;
-with Ada.Containers.Indefinite_Vectors ;
-with Gnat.Awk ;
+with Interfaces;
+with Ada.Strings.Unbounded;
+with Ada.Containers.Indefinite_Vectors;
+with GNAT.AWK;
 
 package Csv is
-   Debug : Boolean := True ;
-   Duplicate_Column : exception ;
-   type File_Object_Type is limited private ;
-   type File_Type is access File_Object_Type ;
-   function Open( Name : String ;
-                  Separator : String ) return File_Type ;
-   procedure Close (File : in out File_Type) ;
-   function No_Columns( File : File_Type ) return Integer ;
-   function Field_Name( File : File_Type ; Column : Integer )
-                      return String ;
-   procedure Get_Line( File : in out File_Type ) ;
-   function Line_No( File : File_Type ) return Integer ;
-   function End_Of_File( File : File_Type )
-                       return Boolean ;
-   function Field( File : File_Type ; Column : Integer )
-                 return String ;
-   function Field( File : File_Type ; Name : String )
-                 return String ;
+   Debug : Boolean := True;
+   Duplicate_Column : exception;
+   type File_Object_Type is limited private;
+   type File_Type is access File_Object_Type;
+   function Open (Name : String; Separator : String) return File_Type;
+   procedure Close (File : in out File_Type);
+   function No_Columns (File : File_Type) return Integer;
+   function Field_Name (File : File_Type; Column : Integer) return String;
+   procedure Get_Line (File : in out File_Type);
+   function Line_No (File : File_Type) return Integer;
+   function End_Of_File (File : File_Type) return Boolean;
+   function Field (File : File_Type; Column : Integer) return String;
+   function Field (File : File_Type; Name : String) return String;
 private
-   subtype Field_Count_Type is Integer Range 1..255 ;
-   package String_Vectors_Pkg is
-     new Ada.Containers.Indefinite_Vectors(
-                                           Index_Type => Field_Count_type ,
-                                           Element_Type => String ) ;
-   type File_Object_Type is limited
-      record
-         Session : Gnat.Awk.Session_Type ;
-         No_Columns : Integer := 0 ;
-         Current_Line : Integer := -1 ;
-         Field_Names : String_Vectors_Pkg.Vector ;
-      end record ;
+   subtype Field_Count_Type is Integer range 1 .. 255;
+   package String_Vectors_Pkg is new Ada.Containers.Indefinite_Vectors
+     (Index_Type   => Field_Count_Type,
+      Element_Type => String);
+   type File_Object_Type is limited record
+      Session      : GNAT.AWK.Session_Type;
+      No_Columns   : Integer := 0;
+      Current_Line : Integer := -1;
+      Field_Names  : String_Vectors_Pkg.Vector;
+   end record;
 
-end Csv ;
+end Csv;
