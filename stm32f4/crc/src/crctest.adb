05520eb9c614ecf6c07bafe4b3e371879946f514
R Srinivasan <s@srin.me>
2015-06-29 21:31:07 -0400

    Example to save crc in a table.

--- stm32f4/crc/src/crctest.adb
+++ stm32f4/crc/src/crctest.adb
@@ -6,9 +6,11 @@ with ada.real_time ; use ada.real_time ;
 with testdata ;
 
 procedure crctest is
+
    use stm32f4 ;
+
    tempcrc : stm32f4.word ;
-   pragma Unreferenced (tempcrc);
+   --pragma Unreferenced (tempcrc);
    block : array (1..11) of stm32f4.word ;
    sleep_time : time_span := milliseconds(500);
    now : time ;
@@ -16,11 +18,11 @@ begin
    STM32F4.CRC.Initialize ;
    tempcrc := stm32F4.crc.compute(System'To_Address(STM32F4.RCC_Base),registers.RCC'Size/8 ) ;
 
-   for byteval in stm32f4.byte'range
+   for byteval in testdata.crctable'range
    loop
       block := (others => word(byteval)) ;
       tempcrc := stm32f4.crc.compute(block'address,block'size/8) ;
-      -- testdata.crctable(integer(byteval)) := tempcrc ;
+      testdata.crctable(byteval) := tempcrc ;
    end loop ;
    loop
       tempcrc := stm32F4.crc.compute(System'To_Address(STM32F4.RCC_Base),registers.RCC'Size/8 ) ;
--- stm32f4/crc/src/testdata.ads
+++ stm32f4/crc/src/testdata.ads
@@ -1,5 +1,6 @@
-pragma Restrictions (No_Elaboration_Code);
+-- pragma Restrictions (No_Elaboration_Code);
 with STM32F4 ;
 package testdata is
+      pragma Elaborate_Body(testdata) ;
    crctable : array (1..11) of STM32F4.Word ;
 end testdata ;
