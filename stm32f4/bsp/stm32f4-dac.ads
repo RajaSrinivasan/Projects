5844856f0d367b626658216c88f7a53f2d952d02
R Srinivasan <s@srin.me>
2015-07-18 22:23:40 -0400

    beginnings of dac support

--- stm32f4/bsp/stm32f4-dac.ads
+++ stm32f4/bsp/stm32f4-dac.ads
@@ -2,28 +2,50 @@ pragma Restrictions (No_Elaboration_Code);
 
 package STM32F4.DAC is
 
+   type Channel_Number_Type is new STM32F4.Bits_1 ;
+
+   type DATAFormat_Type is
+     (
+      RightAligned_8bit ,
+      RightAligned_12bit ,
+      LeftAligned_12bit ) ;
+
+      subtype TriggerEvents_Type is Bits_3 ;
+      Timer6_TRGO : constant TriggerEvents_Type := 2#000# ;
+      Timer8_TRGO : constant TriggerEvents_Type := 2#001# ;
+      Timer7_TRGO : constant TriggerEvents_Type := 2#010# ;
+      Timer5_TRGO : constant TriggerEvents_Type := 2#011# ;
+      Timer2_TRGO : constant TriggerEvents_Type := 2#100# ;
+      Timer4_TRGO : constant TriggerEvents_Type := 2#101# ;
+      EXTI_Line9  : constant TriggerEvents_Type := 2#110# ;
+      SWTRIG      : constant TriggerEvents_Type := 2#111# ;
+
+   procedure Enable( channel : Channel_Number_Type ;
+                     format : DATAFormat_Type ;
+                     Trigger : TriggerEvents_Type ) ;
+
 private
    type DAC_CR_Type is
       record
-         EN1 : Bits_1 ;            -- DAC channel1 enable
-         BOFF1 : Bits_1 ;          -- DAC channel1 output buffer disable
-         TEN1 : Bits_1 ;           -- DAC channel1 trigger enable
-         TSEL1 : Bits_3 ;          -- DAC channel1 trigger selection
-         WAVE1 : Bits_2 ;          -- DAC channel1 noise/triangle wave generation enable
-         MAMP1 : Bits_4 ;          -- DAC channel1 mask/amplitude selector
-         DMAEN1 : Bits_1 ;         -- DAC channel1 DMA enable
-         DMAUDRIE1 : Bits_1 ;      -- DAC channel1 DMA Underrun Interrupt enable
-         Reserved_1 : Bits_2 ;
+         EN1 : Bits_1 := 0 ;            -- DAC channel1 enable
+         BOFF1 : Bits_1 := 0 ;          -- DAC channel1 output buffer disable
+         TEN1 : Bits_1 := 0 ;           -- DAC channel1 trigger enable
+         TSEL1 : Bits_3 := 0;          -- DAC channel1 trigger selection
+         WAVE1 : Bits_2 := 0;          -- DAC channel1 noise/triangle wave generation enable
+         MAMP1 : Bits_4 := 0;          -- DAC channel1 mask/amplitude selector
+         DMAEN1 : Bits_1 := 0 ;         -- DAC channel1 DMA enable
+         DMAUDRIE1 : Bits_1 := 0;      -- DAC channel1 DMA Underrun Interrupt enable
+         Reserved_1 : Bits_2 := 0;
 
-         EN2 : Bits_1 ;            -- DAC channel2 enable
-         BOFF2 : Bits_1 ;          -- DAC channel2 output buffer disable
-         TEN2 : Bits_1 ;           -- DAC channel2 trigger enable
-         TSEL2 : Bits_3 ;          -- DAC channel2 trigger selection
-         WAVE2 : Bits_2 ;          -- DAC channel2 noise/triangle wave generation enable
-         MAMP2 : Bits_4 ;          -- DAC channel2 mask/amplitude selector
-         DMAEN2 : Bits_1 ;         -- DAC channel2 DMA enable
-         DMAUDRIE2 : Bits_1 ;      -- DAC channel2 DMA Underrun Interrupt enable
-         Reserved_2 : Bits_2 ;
+         EN2 : Bits_1 := 0;            -- DAC channel2 enable
+         BOFF2 : Bits_1 := 0;          -- DAC channel2 output buffer disable
+         TEN2 : Bits_1 := 0;           -- DAC channel2 trigger enable
+         TSEL2 : Bits_3 := 0;          -- DAC channel2 trigger selection
+         WAVE2 : Bits_2 := 0;          -- DAC channel2 noise/triangle wave generation enable
+         MAMP2 : Bits_4 := 0;          -- DAC channel2 mask/amplitude selector
+         DMAEN2 : Bits_1 := 0;         -- DAC channel2 DMA enable
+         DMAUDRIE2 : Bits_1 := 0;      -- DAC channel2 DMA Underrun Interrupt enable
+         Reserved_2 : Bits_2 := 0;
 
       end record ;
    for DAC_CR_Type'Size use Word'Size ;
@@ -62,6 +84,73 @@ private
          SWTRIG1 at 0 range 0..0 ;
          SWTRIG2 at 0 range 1..1 ;
          Reserved_1 at 0 range 2..15 ;
+         Reserved_2 at 2 range 0..15 ;
+      end record ;
+   type DAC_DHR12R1_Type is
+      record
+         DACC1DHR : Bits_12 ;
+         Reserved_1 : Bits_4 ;
+         Reserved_2 : Half_Word ;
+      end record ;
+   for DAC_DHR12R1_Type'Size use Word'Size ;
+   for DAC_DHR12R1_Type use
+      record
+         DACC1DHR at 0 range 0..11 ;
+         Reserved_1 at 0 range 12..15 ;
+         Reserved_2 at 2 range 0..15 ;
+      end record ;
+   type DAC_DHR12L1_Type is
+      record
+         Reserved_1 : Bits_4 ;
+         DACC1DHR : Bits_12 ;
+         Reserved_2 : Half_Word ;
+      end record ;
+   for DAC_DHR12L1_Type'Size use 32 ;
+   for DAC_DHR12L1_Type use
+      record
+         Reserved_1 at 0 range 0..3 ;
+         DACC1DHR at 0 range 4..15 ;
+         Reserved_2 at 2 range 0..15 ;
+      end record ;
+   type DAC_DHR8R1_Type is
+      record
+         DACC1DHR : Bits_8 ;
+         Reserved_1 : Bits_8 ;
+         Reserved_2 : Half_Word ;
+      end record ;
+   for DAC_DHR8R1_Type'Size use Word'Size ;
+   for DAC_DHR8R1_Type use
+      record
+         DACC1DHR at 0 range 0..7 ;
+         Reserved_1 at 0 range 8..15 ;
+         Reserved_2 at 2 range 0..15 ;
+      end record ;
+
+   type DAC_DOR1_Type is
+      record
+         DACC1DOR : Bits_12 ;
+         Reserved_1 : Bits_4 ;
+         Reserved_2 : Half_Word ;
+      end record ;
+   for DAC_DOR1_Type'Size use Word'Size ;
+   for DAC_DOR1_Type use
+      record
+         DACC1DOR at 0 range 0..11 ;
+         Reserved_1 at 0 range 12..15 ;
+         Reserved_2 at 2 range 0..15 ;
+      end record ;
+
+   type DAC_DOR2_Type is
+      record
+         DACC2DOR : Bits_12 ;
+         Reserved_1 : Bits_4 ;
+         Reserved_2 : Half_Word ;
+      end record ;
+   for DAC_DOR2_Type'Size use Word'Size ;
+   for DAC_DOR2_Type use
+      record
+         DACC2DOR at 0 range 0..11 ;
+         Reserved_1 at 0 range 12..15 ;
          Reserved_2 at 2 range 0..15 ;
       end record ;
 
