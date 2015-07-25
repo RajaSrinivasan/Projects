1ab23b3e7b988f8764173e2f3bfb4ce7d3b22ad2
R Srinivasan <s@srin.me>
2015-07-25 11:50:02 -0400

    reorganized symbol table. added a new utility createstb

--- PreProc/preprocessor.ads
+++ PreProc/preprocessor.ads
@@ -1,11 +1,4 @@
-with ada.strings.unbounded ; use ada.strings.unbounded ;
-with ada.strings.unbounded.Less_Case_Insensitive ;
-
-with Ada.Containers.Indefinite_Ordered_Maps ;
-
 package PreProcessor is
-    package stb_pkg is new Ada.Containers.Indefinite_Ordered_Maps(
-                                                                   unbounded_string , unbounded_string ,
-                                                                   "<" => ada.strings.unbounded.Less_Case_Insensitive ,
-                                                                   "=" => ada.strings.unbounded."=" ) ;
+   Verbose : Boolean := True ;
+   procedure Initialize ;
 end PreProcessor ;
