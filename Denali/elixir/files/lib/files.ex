c778457af8f50ace50f897a40df86bf71e7d1249
RajaSrinivasan <s@srin.me>
2015-10-10 06:55:20 -0400

    digest with different algorithms in parallel

--- Denali/elixir/files/lib/files.ex
+++ Denali/elixir/files/lib/files.ex
@@ -10,6 +10,28 @@ defmodule Files do
                           lnum
                       end)
    end
+
+   def digest_task (alg) do
+       # IO.puts("Will use algorithm #{alg}")
+       receive do
+          fname ->
+
+                       mydigest = digest(fname,alg)
+                       IO.puts("File #{fname} Algorithm #{alg} Digest: #{mydigest}")
+       end
+   end
+
+   def digest_tasks(fname) do
+       algorithms=[:md5,:ripemd160,:sha,:sha224,:sha256,:sha384,:sha512]
+       alg=0
+       Enum.reduce(:crypto.supports, alg ,
+                               fn(x,acc) ->
+                                   pid=spawn(Files,:digest_task,[x])
+                                   send(pid,fname)
+                               end
+                               )
+   end
+
    def digest(filename) do
        digest(filename,:md5)
    end
@@ -19,4 +41,5 @@ defmodule Files do
        mydigest = :crypto.hash_update(mydigest,fcontents)
        :crypto.hash_final(mydigest) |> Base.encode16 |> String.downcase
    end
+
 end
