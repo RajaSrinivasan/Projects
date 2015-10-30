700d990072b06514ea83bf0480b428062025d0d5
RajaSrinivasan <s@srin.me>
2015-10-29 21:35:17 -0400

    line count of files added

--- Denali/elixir/files/lib/files.ex
+++ Denali/elixir/files/lib/files.ex
@@ -12,15 +12,57 @@ defmodule Files do
        lines
    end
 
+   def linecount([],table) do
+       table
+   end
+
+   def linecount([head | rest], table) do
+       tbl=linecount(head,table)
+       linecount(rest,tbl)
+   end
+
    def linecount(filename, table) do
-       lc=linecount(filename)
-       ext=Path.extname(filename)
-       if Dict.has_key?(table,ext) do
-          IO.puts(ext)
-          IO.puts("already exists")
+       if not File.regular?(filename) do
+          # IO.puts("#{filename} is not a regular file")
+          linecountdir(filename,table)
        else
-          Dict.put(table,ext,lc)
+         lc=linecount(filename)
+         # IO.puts("File #{filename} lines #{lc}")
+         ext=Path.extname(filename)
+         if Dict.has_key?(table,ext) do
+            # IO.puts(ext)
+            # IO.puts("already exists")
+            {oldfiles,oldval}=Dict.get(table,ext)
+            newval=oldval+lc
+            Dict.put(table,ext,{oldfiles+1,newval})
+         else
+            # IO.puts("Adding filetype #{ext} linecount #{lc}")
+            Dict.put(table,ext,{1,lc})
+         end
        end
+   end
+
+   def linecountdir(dirname) do
+       table=HashDict.new()
+       table=Enum.sort(linecountdir(dirname,table))
+       Enum.reduce(table,[],fn(x,acc) ->
+                          {name,stats}=x
+                          {filecount,lines}=stats
+                          IO.puts("#{name}\t\t: #{filecount} \t #{lines}")
+                          end)
+   end
+   def linecountdir(dirname,table) do
+     {status , files} = File.ls(dirname)
+     if status == :ok do
+        filelist=Enum.reduce(files,[], fn(x, acc) ->
+                                       List.flatten([Path.join(Path.absname(dirname),x) , acc])
+                                 end
+                            )
+        tbl=linecount( filelist , table )
+     else
+        IO.puts("Error for #{dirname} - #{status}")
+        tbl=table
+     end
    end
 
    def show(filename) do
@@ -39,7 +81,6 @@ defmodule Files do
        # IO.puts("Will use algorithm #{alg}")
        receive do
           fname ->
-
                        mydigest = digest(fname,alg)
                        IO.puts("Algorithm #{alg} Digest: #{mydigest}")
        end
@@ -59,7 +100,7 @@ defmodule Files do
    def digest(filename) do
        digest(filename,:md5)
    end
-   
+
    def digest(filename,alg) do
        fcontents=File.read!(filename)
        mydigest = :crypto.hash_init(alg)
