6ea634af2c4a7ded200dcf5a354c5d7c7087c9d5
R Srinivasan <s@srin.me>
2015-12-26 15:35:59 -0500

    Added a progress bar for file uploads

--- PyQt/install/startup.py
+++ PyQt/install/startup.py
@@ -1,11 +1,14 @@
 from PyQt5 import QtCore
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from PyQt5.QtNetwork import *
+
 from PyQt5.QtWidgets import (QWidget,
                              QToolTip,
                              QPushButton,
                              QApplication,
-                             QDialog)
+                             QDialog,
+                             QStyle )
 from PyQt5 import uic
 
 class ConsoleIP(QDialog):
@@ -13,12 +16,18 @@ class ConsoleIP(QDialog):
         super().__init__()
         ipadrrexp = QRegExp("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
         ipadrvalidator = QRegExpValidator(ipadrrexp)
-
+        self.counter=0
         self.ui = uic.loadUi("startup.ui")
         self.ui.txtConsoleIPAddress.setValidator(ipadrvalidator)
 
         self.ui.btnCancel.clicked.connect(self.CancelClicked)
         self.ui.btnConnect.clicked.connect(self.ConnectClicked)
+
+        self.timer = QTimer()
+        self.ui.pbTransferFiles.setValue(0)
+        self.ui.pbTransferFiles.hide()
+
+        self.timer.timeout.connect(self.TimerTicks)
         self.ui.show()
 
     @pyqtSlot()
@@ -27,5 +36,19 @@ class ConsoleIP(QDialog):
 
     @pyqtSlot()
     def ConnectClicked(self):
-        print("Connect to the console %s" % self.ui.txtConsoleIPAddress.text())
-        self.ui.done(1)
+        if self.counter > 0:
+            self.ui.done(1)
+        else:
+            self.ui.pbTransferFiles.show()
+            self.ui.lblInstruction.setText("Please wait for file Uploads")
+            self.ui.btnConnect.setEnabled(False)
+            self.timer.start(1000)
+
+    @pyqtSlot()
+    def TimerTicks(self):
+        self.counter += 10
+        if self.counter > 100:
+            self.ui.btnConnect.setText("Proceed")
+            self.ui.btnConnect.setEnabled(1)
+        else:
+            self.ui.pbTransferFiles.setValue(self.counter)
