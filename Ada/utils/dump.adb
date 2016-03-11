3537d9fce26a88329cab403cf69d2fed294bac45
R Srinivasan <s@srin.me>
2016-03-11 10:25:57 -0800

    dump utility cloned from template

---
+++ Ada/utils/dump.adb
@@ -0,0 +1,5 @@
+with dump_cli ;
+procedure dump is
+begin
+   dump_cli.ProcessCommandLine ;
+end dump ;
--- Ada/utils/dump_cli.ads
+++ Ada/utils/dump_cli.ads
@@ -1,10 +1,9 @@
 with Ada.Strings.Unbounded ; use Ada.Strings.Unbounded ;
 with gnat.strings ;
 
-package cli is
+package dump_cli is
    verbose : aliased boolean := false ;              -- Option:     -v or --verbose
    Arg : unbounded_string := null_unbounded_string ;
    outputname : aliased gnat.strings.string_access ;
-   procedure ShowUsage ;                     -- called when -h or --help
    procedure ProcessCommandLine ;
-end cli ;
+end dump_cli ;
