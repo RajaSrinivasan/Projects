d1861c0e1872a065a36dfefba150e1461c6bb9e2
R Srinivasan <s@srin.me>
2016-03-12 16:54:23 -0800

    All template files processed

--- utils/newutil_pkg.ads
+++ utils/newutil_pkg.ads
@@ -1,9 +1,10 @@
+with Ada.Strings.Unbounded ; use Ada.Strings.Unbounded ;
 package newutil_pkg is
     defaultconfig : string :=
     "{"
     & "    ""newutil"" : " & ASCII.LF
     & "       {" & ASCII.LF
-    & "         ""templatedir"" : ""$ROOT/templates"" , " & ASCII.LF
+    & "         ""templatedir"" : ""template/"" , " & ASCII.LF
     & "         ""templates"" : " & ASCII.LF
     & "           { " & ASCII.LF
     & "              ""cli.ads"" : ""/$NEWPROJ/_cli.ads"" , " & ASCII.LF
@@ -13,6 +14,17 @@ package newutil_pkg is
     & "           } " & ASCII.LF
     & "      } " & ASCII.LF
     & "}" ;
+
+    templatedir : unbounded_string := null_unbounded_string ;
+    numtemplates : integer := 0 ;
+    templates : array (1..16) of unbounded_string := (others => null_unbounded_string) ;
+    template_values : array (1..16) of unbounded_string := (others => null_unbounded_string) ;
+
     procedure show_config ;
     procedure load_config( filename : string ) ;
+    procedure process_file( filename : string ) ;
+    procedure process_file( filename : string ;
+                            outputfilename : string ;
+                            progname : string ) ;
+    procedure ParseConfig ;
 end newutil_pkg ;
