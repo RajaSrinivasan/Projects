84cdf4f3c91185797c404606599cae55f267fad4
R Srinivasan <s@srin.me>
2016-04-02 09:06:20 -0400

    added the file method

--- Python/pyctest/ctestmodule.c
+++ Python/pyctest/ctestmodule.c
@@ -12,10 +12,25 @@ ctest_system(PyObject *self, PyObject *args)
     return PyLong_FromLong(sts);
 }
 
+static PyObject *
+ctest_file(PyObject *self, PyObject *args)
+{
+    const char *filename;
+
+    if (!PyArg_ParseTuple(args, "s", &filename))
+        return NULL;
+    printf("Opening file %s\n" , filename);
+
+    return PyLong_FromLong(0);
+}
+
+
 static PyMethodDef ctestMethods[] = {
     {"system",  ctest_system, METH_VARARGS,
      "Execute a shell command."},
-    {NULL, NULL, 0, NULL}        /* Sentinel */
+     {"file",  ctest_file, METH_VARARGS,
+      "Open a special file."},
+          {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
 static struct PyModuleDef ctestmodule = {
