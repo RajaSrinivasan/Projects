19afa094ba6ead757858a526ba2852c5d17758de
RajaSrinivasan <s@srin.me>
2016-08-14 18:04:33 -0400

    removed empty files

--- Ada/queue/client/.adb
+++
@@ -1,5 +0,0 @@
-with _cli ;                            -- [cli/$_cli]
-procedure  is                  -- [clitest/$]
-begin
-   _cli.ProcessCommandLine ;           -- [cli/$_cli]
-end  ;                         -- [clitest/$]
