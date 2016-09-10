e65ff7a399bde7da9fba7dba8c9e71a1d16f9c14
rsrinivasan <rsrinivasan@abiomed.com>
2016-09-10 17:37:03 -0400

    removed template files from obsolete loc

--- template/clitest.adb
+++
@@ -1,5 +0,0 @@
-with cli ;                            -- [cli/$_cli]
-procedure clitest is                  -- [clitest/$]
-begin
-   cli.ProcessCommandLine ;           -- [cli/$_cli]
-end clitest ;                         -- [clitest/$]
