8451f8271f03e23b5ad89384698ae55ea4e869fe
vagrant <vagrant@vagrant.vm>
2017-04-16 21:54:34 +0000

    Tutor removed

--- Tutor/cleanupmodalities.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'srini'
-from tutordbsetup import Modality, deletemodality
-
-for m in range (8,15):
-    deletemodality(m)
--- Tutor/listmodalities.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'srini'
-from tutordbsetup import Modality, listmodalities, listmodalitiesJSON
-#from flask import Flask
-#app = Flask(__name__)
-
-ms = listmodalities()
-for m in ms:
-    print(m.name, "", m.id)
-
