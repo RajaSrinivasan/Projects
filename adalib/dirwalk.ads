9ec26664936eac8390cffe378864545955426689
srini <RajaSrinivasan@hotmail.com>
2017-04-16 21:59:17 +0000

    obsolete adalib removed

--- adalib/dirwalk.ads
+++
@@ -1,8 +0,0 @@
-generic
-    type context_type is private ;
-procedure dirwalk( context : context_type ;
-                   dirname : string ;
-                   pattern : string ;
-                   processor : access procedure ( context : context_type ;
-                                                  filename : in string )
-                   ) ;
--- adalib/long_long_numbers.ads
+++
@@ -1,3 +0,0 @@
-with numbers ;
-package long_long_numbers is new numbers(
-        number_type => long_long_integer) ;
